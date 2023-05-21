6eda834bd0fbe671d3364757890e5b778cee5ae8
ThibFrgsGmz <farges.thibault@gmail.com>
2023-05-21 19:52:43 +1200

    (add) rand dep + usage example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rand::prelude::*;
+
 fn main() {
     println!("Hello, world!");
+
+    if rand::random() { // generates a boolean
+        println!("char: {}", rand::random::<char>());
+    }
+
+    let mut rng = rand::thread_rng();
+    let _y: f64 = rng.gen(); // generates a float between 0 and 1
+
+    let mut nums: Vec<i32> = (1..100).collect();
+    nums.shuffle(&mut rng);
 }

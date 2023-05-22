349918c6314c4e343a9a27084e01d2b535793d7c
ThibFrgsGmz <farges.thibault@gmail.com>
2023-05-22 21:16:22 +1200

    (add) UT while refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,64 @@
-use rand::prelude::*;
+mod display;
 mod grid;
 mod rules;
-mod display;
 mod simulation;
+use rand::prelude::*;
 
 fn main() {
     println!("Hello, world!");
 
-    if rand::random() { // generates a boolean
-        println!("char: {}", rand::random::<char>());
+    if let Some(c) = generate_random_char() {
+        println!("char: {}", c);
     }
 
-    let mut rng = rand::thread_rng();
-    let _y: f64 = rng.gen(); // generates a float between 0 and 1
+    let _y = generate_random_float();
 
+    let _nums = generate_random_array();
+}
+
+fn generate_random_char() -> Option<char> {
+    if rand::random() {
+        // generates a boolean
+        Some(rand::random::<char>()) // generates a random character
+    } else {
+        None
+    }
+}
+
+fn generate_random_float() -> f64 {
+    let mut rng = rand::thread_rng();
+    rng.gen() // generates a float between 0 and 1
+}
+
+fn generate_random_array() -> Vec<i32> {
+    let mut rng = rand::thread_rng();
     let mut nums: Vec<i32> = (1..100).collect();
     nums.shuffle(&mut rng);
+    nums
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_random_char() {
+        let c = generate_random_char();
+        assert!(c.is_none() || c.unwrap().is_alphabetic());
+    }
+
+    #[test]
+    fn test_generate_random_float() {
+        let f = generate_random_float();
+        assert!(f >= 0.0 && f <= 1.0);
+    }
+
+    #[test]
+    fn test_generate_random_array() {
+        let nums = generate_random_array();
+        assert_eq!(nums.len(), 99);
+        for i in 1..100 {
+            assert!(nums.contains(&i));
+        }
+    }
 }

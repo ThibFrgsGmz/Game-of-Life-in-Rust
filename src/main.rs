e8f8b7510dff2ca7ff6d226e561310cf37e12cd6
ThibFrgsGmz <farges.thibault@gmail.com>
2023-06-04 18:41:38 +1200

    (feat) random assign of grid

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rand::distributions::{Distribution, Uniform};
-use rand::Rng;
+// use rand::Rng;
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Cell {
     Dead,
     Live,
@@ -22,44 +22,40 @@ pub fn calculate(op: char, a: i64, b: i64) -> Option<i64> {
         _ => None,
     }
 }
-
 fn main() {
     println!("Hello, world!");
 
-    let op = '+';
-    let a = 5;
-    let b = 3;
-    match calculate(op, a, b) {
-        Some(result) => println!("Result: {}", result),
-        None => println!("Error: invalid operation or division by zero"),
-    }
+    // let op = '+';
+    // let a = 5;
+    // let b = 3;
+    // match calculate(op, a, b) {
+    //     Some(result) => println!("Result: {}", result),
+    //     None => println!("Error: invalid operation or division by zero"),
+    // }
 
     let width = 10;
     let height = 20;
-
     let mut array = vec![vec![Cell::Dead; width]; height];
 
     let mut rng = rand::thread_rng();
     let cell_dist = Uniform::from(0..2);
 
-    for (i, row) in array.iter().enumerate() {
-        for (j, _cell) in row.iter().enumerate() {
-            let mut _xx = array[i][j];
-            _xx = match cell_dist.sample(&mut rng) {
+    (0..height).for_each(|i| {
+        (0..width).for_each(|j| {
+            array[i][j] = match cell_dist.sample(&mut rng) {
                 0 => Cell::Dead,
                 _ => Cell::Live,
             };
-        }
-    }
+            println!("Cell at ({}, {}) is {:?}", i, j, array[i][j]);
+        });
+    });
 
-    for (i, row) in array.iter().enumerate() {
-        for (j, cell) in row.iter().enumerate() {
-            match cell {
-                Cell::Dead => println!("Cell at ({}, {}) is Dead", i, j),
-                Cell::Live => println!("Cell at ({}, {}) is Alive", i, j),
-            }
-        }
-    }
+    array.iter().enumerate().for_each(|(i, row)| {
+        row.iter().enumerate().for_each(|(j, cell)| match cell {
+            Cell::Dead => println!("Cell at ({}, {}) is Dead", i, j),
+            _ => println!("Cell at ({}, {}) is Alive", i, j),
+        });
+    });
 }
 
 // fn generate_random_float() -> f64 {
